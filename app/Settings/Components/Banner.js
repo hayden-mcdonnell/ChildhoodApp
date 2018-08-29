8b3cef3536edfdde201c16bea70ac4db8ef3da05
Hayden McDonnell <hayden.mcdonnell@hotmail.com>
2018-08-29 11:58:30 +1000

    Image sent client sided

--- app/Settings/Components/Banner.js
+++ app/Settings/Components/Banner.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, StyleSheet, Text } from 'react-native';
+import { AppRegistry, View, StyleSheet, Text, Image, Button } from 'react-native';
+
+import CameraRollPicker from 'react-native-camera-roll-picker';
 
 export default class Banner extends Component {
+     constructor(props) {
+        super(props);
+
+      }
+
+      openRoll = () =>{
+          this.props.camera();
+      }
+      
     render() {
         return (
                 <View style={{flex: 2}}> //This controlls the banners height
-                <View style={styles.bg}>
-                
-                
-                // Banner info goes here
-                
-                
-                </View>
+                    <View style={styles.bg}>
+                    
+                   
+                    <Button onPress={this.openRoll} title={'Open modal'} color={'red'}/>
+                    </View>
                 </View>
                 );
     }
@@ -19,13 +28,19 @@ export default class Banner extends Component {
 
 const styles = StyleSheet.create({
 
-                                 bg:
-                                 {
-                                 flex: 2, //flex here also controlls banners height
-                                 backgroundColor: '#005691'
-                                 
-                                 },
-                                 
+    bg:
+    {
+        flex: 2, //flex here also controlls banners height
+        backgroundColor: '#005691',
+        justifyContent: 'center',
+        alignItems: 'center'
+     },
+    imageLocation:
+    {
+        marginTop: 20,
+        marginBottom: 20,
+        borderRadius: 50
+    }
 
 
-})
+});
--- app/Settings/Settings.js
+++ app/Settings/Settings.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View,} from 'react-native';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
 import Header from '../GlobalComponents/Header';
 import Navigation from '../GlobalComponents/Navigation';
 import Options from './Components/OptionsList'; //Settings option view will go in here
 import Banner from './Components/Banner';
+import CameraRollPicker from 'react-native-camera-roll-picker';
+
 
 export default class homepage extends Component{
     
@@ -12,23 +14,61 @@ export default class homepage extends Component{
         super(props);
 
         this.state = {
-            userId: this.props.navigation.getParam('UserData', 'NO-Data')
+            userId: this.props.navigation.getParam('UserData', 'NO-Data'),
+            cameraRoll: false,
+            image: {}
         };
+    }
+
+    cameraRollChange = () => {
+        this.setState({
+            cameraRoll: true
+        });
+    }
+
+    getSelectedImages = (images, current) => {
+        this.setState({
+            image: current
+        })
+        console.log(current);
+    }
+
+    sendPhoto = () =>{
+        fetch('http://localhost:3000/api/profilePic', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.state.image),
+            });
+        console.log(this.state.image);
     }
     
     render() {
-        
+        const normal = <View style={styles.Main}> //Flex needs to be 1 here to set menu bar to bottom
+                
+                        
+                    
+                        <Banner camera={this.cameraRollChange}/>
+                        <Options user={this.state.userId}/>
+                            
+                            
+                            <Navigation nav={this.props.navigation} user={this.state.userId}/>
+                            </View>;
+
+            const picker = <View style={styles.Main}>
+                               <CameraRollPicker callback={this.getSelectedImages} selectSingleItem={true}/>
+                               <Button onPress={this.sendPhoto} title={'Confirm'} color={'green'}/>
+                            </View>;
+
+
+
         return (
-                <View style={styles.Main}> //Flex needs to be 1 here to set menu bar to bottom
-                <View>
-                <Header title='Settings'/>
-                </View>
-               
-               <Banner />
-               <Options user={this.state.userId}/>
-                
-                
-                <Navigation nav={this.props.navigation} user={this.state.userId}/>
+                <View style={styles.Main}>
+                    <View>
+                        <Header title='Settings'/>
+                    </View>
+                    {this.state.cameraRoll ? picker : normal}
                 </View>
                 );
     }
